7e8c57e867aa1e177baed4c10e1fbc9cb4324c1f
ggim00922 <ggim00922@gmail.com>
2018-07-06 10:53:46 +0900

    07061053

--- JejuLantis/src/main/java/com/khd/rentcarController/RentcarController.java
+++ JejuLantis/src/main/java/com/khd/rentcarController/RentcarController.java
@@ -20,25 +20,6 @@ public class RentcarController {
 	
 	@RequestMapping(value="car.do",method=RequestMethod.GET)
 	public String car(HttpSession session) {
-/*		HashMap<String,String> map = new HashMap<String,String>();
-		map.put("cartype","스팅어");
-		map.put("menufactur","기아");
-		map.put("perwage","26400");
-		map.put("passenger","5");
-		map.put("fuel","휘발유");
-		map.put("trans","오토");
-		map.put("best","특별한렌트카");
-		map.put("option1","옵션1");
-		map.put("option2","옵션2");
-		map.put("option3","옵션3");
-		map.put("option4","옵션4");
-		map.put("option5","옵션5");
-		map.put("branch","특별한렌트카");
-		map.put("rating","4.1");
-		map.put("xx","13400");
-		map.put("insurance","13000");
-		map.put("yy","2000만원");
-		map.put("cartype","26400");*/
 		List list = rentcarservice.searchService();
 		System.out.println(list.size());
 		session.setAttribute("list", list);
@@ -47,11 +28,6 @@ public class RentcarController {
 	
 	@RequestMapping(value="car.do",method=RequestMethod.POST)
 	public String car(@RequestParam("Checkouttime")String checkouttime,@RequestParam("Checkoutdate")String checkoutdate,@RequestParam("Checkintime")String checkintime,@RequestParam("Checkindate")String checkindate,@RequestParam("car_kind_name")String car_kind_name) {
-/*		System.out.println(checkindate);
-		System.out.println(checkintime);
-		System.out.println(checkoutdate);
-		System.out.println(checkouttime);
-		System.out.println(car_kind_name);*/
 
 		return "rentcar/car";
 	}

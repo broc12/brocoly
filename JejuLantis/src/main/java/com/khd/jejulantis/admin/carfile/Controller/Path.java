d26b7721b4151173b5b18140cd1eba0aeb613998
5CLASS-184 <5CLASS-184@BIT05-14>
2018-08-18 13:26:36 +0900

    이미지 경로 변경 C:/jejuLantis/rentcar/img

--- JejuLantis/src/main/java/com/khd/jejulantis/admin/carfile/Controller/Path.java
+++ JejuLantis/src/main/java/com/khd/jejulantis/admin/carfile/Controller/Path.java
@@ -1,5 +1,5 @@
 package com.khd.jejulantis.admin.carfile.Controller;
 
 public class Path {
-	public static final String FILE_STORE="C:/project/jejuLantis/rentcar/img";
+	public static final String FILE_STORE="C:/jejuLantis/rentcar/img";
 }
